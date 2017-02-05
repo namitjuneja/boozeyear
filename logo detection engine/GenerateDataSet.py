d831ad5a521cd0c225345bacc29aa5c80e463550
namitjuneja <juneja.namit@gmail.com>
2017-02-05 12:16:53 +0530

    logo detection engine

---
+++ logo detection engine/GenerateDataSet.py
@@ -0,0 +1,23 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+img = load_img('/Users/aditya/Desktop/data/train/becks/becks_0.jpg')  # this is a PIL image
+x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+i = 0
+for batch in datagen.flow(x, batch_size=1,
+                          save_to_dir='data/train/becks', save_prefix='becks', save_format='png'):
+    i += 1
+    if i > 100:
+        break  # otherwise the generator would loop indefinitely
